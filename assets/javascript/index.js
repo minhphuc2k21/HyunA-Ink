1fa4a2536d67a3f1197f2c4f4078701068715927
ColeColombia <benzemabenelzane@yahoo.com>
2022-03-16 23:41:19 +0200

    Automatically update footer year

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,1 +1,9 @@
+$(document).ready(function(){
 
+  let date = new Date();
+  let year = date.getFullYear();
+
+  $(".footer").html(`Copyright &copy; Tattoo Shop
+   ${year} all rights reserved`);
+
+});
