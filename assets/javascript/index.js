ab1d34709af7c2b9a3e7dfdc8f2bf958df687ed4
ColeColombia <Benzemabenelzane@yahoo.com>
2022-06-11 03:26:22 +0200

    update pictures

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   let date = new Date();
   let year = date.getFullYear();
 
-  $(".footer").html(`Copyright &copy; Tattoo Shop
+  $(".footer").html(`Copyright &copy; Ink Master
    ${year} all rights reserved`);
 
 });
